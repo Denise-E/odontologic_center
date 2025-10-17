32b542ae811ecb37a8b62855417bbf871fe24dd3
Denise Eichenblat <denise.eichenblat@gmail.com>
2025-10-16 21:22:13 -0300

    replaceing syso with loggers

--- src/main/java/up/edu/microservicios/controller/PacienteController.java
+++ src/main/java/up/edu/microservicios/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package up.edu.microservicios.controller;
 
+import org.apache.log4j.Logger;
 import up.edu.microservicios.model.Paciente;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import up.edu.microservicios.service.PacienteService;
 
+import java.util.List;
+
 @RestController // sin vista
 @RequestMapping("/api/pacientes") // todo lo que venga de endpoints de pacientes
 public class PacienteController {
+    private static final Logger LOGGER= Logger.getLogger(PacienteController.class);
     private PacienteService pacienteService;
 
     // @Autowired
@@ -22,12 +26,22 @@ public class PacienteController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Paciente> buscarPorId(@PathVariable Integer id){
-        System.out.println("Buscando paciente por ID: "+id);
+        LOGGER.info("Buscando paciente por ID: "+id);
         Paciente pacienteBuscado = pacienteService.buscarPorId(id);
 
-        System.out.println("Paciente buscado: " +pacienteBuscado);
+        LOGGER.info("Paciente encontrado: "+pacienteBuscado);
         if(pacienteBuscado != null){
             return ResponseEntity.ok(pacienteBuscado);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Paciente>> buscarTodos(){
+        LOGGER.info("Buscando todos pacientes");
+        List<Paciente> pacientes = pacienteService.buscarTodos();
+        if(pacientes != null){
+            return ResponseEntity.ok(pacientes);
         }
         return ResponseEntity.notFound().build();
     }
