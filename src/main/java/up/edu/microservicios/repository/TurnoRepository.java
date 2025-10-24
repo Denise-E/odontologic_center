e600c72fef6d004f201f48b243d0322692f34856
Denise Eichenblat <denise.eichenblat@gmail.com>
2025-10-23 21:55:54 -0300

    updating comments

--- src/main/java/up/edu/microservicios/repository/TurnoRepository.java
+++ src/main/java/up/edu/microservicios/repository/TurnoRepository.java
@@ -5,13 +5,8 @@ import up.edu.microservicios.entity.Turno;
 
 import java.util.List;
 
-// Jpa = Java Persistent APi
 public interface TurnoRepository extends JpaRepository<Turno, Integer> {
-    // JpaRepository ya provee:
-    // - findById(Integer id): Optional<Turno>
-    // - findAll(): List<Turno>
-    // - save(Turno entity): Turno
-    // - deleteById(Integer id): void
+
     
     // Métodos personalizados para buscar turnos
     List<Turno> findByPacienteId(Integer pacienteId);
