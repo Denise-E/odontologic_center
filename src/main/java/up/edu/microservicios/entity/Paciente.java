16942cc5f652d21d39ed714791ee9b27ea670e18
Denise Eichenblat <denise.eichenblat@gmail.com>
2025-10-30 21:16:57 -0300

    patient adjustments

--- src/main/java/up/edu/microservicios/entity/Paciente.java
+++ src/main/java/up/edu/microservicios/entity/Paciente.java
@@ -1,5 +1,6 @@
 package up.edu.microservicios.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,6 +30,7 @@ public class Paciente {
     private String email;
 
     @OneToMany(mappedBy = "paciente", fetch = FetchType.LAZY)
+    @JsonIgnore
     private Set<Turno> turnos = new HashSet<>(); // Set porque son datos únicos no duplicados
 
     public Paciente(Integer id, String nombre, String apellido, String numeroContacto, LocalDate fechaIngreso, Domicilio domicilio, String email) {
